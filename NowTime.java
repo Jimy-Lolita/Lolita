5daac74db150cffd432357e1e5158e31be3d725c
Trrinity <522268397@qq.com>
2016-10-07 13:29:43 +0800

    Update NowTime.java

--- NowTime.java
+++ NowTime.java
@@ -5,23 +5,26 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.Timer;
 import javax.swing.JFrame;
 
-public class NowTime extends JFrame {
-
+public class NowTime extends  JPanel {
+	// 添加 显示时间的JLabel
 	public NowTime() {
 		JLabel time = new JLabel();
 		add(time);
 		this.setTimer(time);
 	}
 
+	// 设置Timer 1000ms实现一次动作 实际是一个线程
 	private void setTimer(JLabel time) {
 		final JLabel varTime = time;
 		Timer timeAction = new Timer(1000, new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
 				long timemillis = System.currentTimeMillis();
+				// 转换日期显示格式
 				SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 				varTime.setText(df.format(new Date(timemillis)));
 			}
@@ -29,9 +32,9 @@ public class NowTime extends JFrame {
 		timeAction.start();
 	}
 
+	// 运行方法
 	public static void main(String[] args) {
 		NowTime timeFrame = new NowTime();
-		timeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		timeFrame.setSize(160, 80);
 		timeFrame.setVisible(true);
 
