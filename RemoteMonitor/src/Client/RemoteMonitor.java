acf8e1208573a31fef75cd7970e0b9ba6103e030
Trrinity <522268397@qq.com>
2016-10-26 21:18:03 +0800

    RemoteMonitor Filechoose

---
+++ RemoteMonitor/src/Client/RemoteMonitor.java
@@ -0,0 +1,66 @@
+package Client;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+
+import ServerWhat.Constant;
+
+public class RemoteMonitor extends Thread {
+	private Dimension screenSize;
+	private Rectangle rectangle;
+	private Robot robot;
+
+	public RemoteMonitor() {
+		screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		rectangle = new Rectangle(screenSize);// 可以指定捕获屏幕区域
+		try {
+			robot = new Robot();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e);
+		}
+	}
+
+	public void run() {
+		ZipOutputStream os = null;
+		Socket socket = null;
+		while (true) {
+			try {
+				socket = new Socket(Constant.ADDR, 5001);// 连接远程IP
+				BufferedImage image = robot.createScreenCapture(rectangle);// 捕获制定屏幕矩形区域
+				os = new ZipOutputStream(socket.getOutputStream());// 加入压缩流
+
+				os.setLevel(9);
+				os.putNextEntry(new ZipEntry("test.jpg"));
+				JPEGCodec.createJPEGEncoder(os).encode(image);// 图像编码成JPEG
+				os.flush();
+				System.out.println("write");
+				os.close();
+				Thread.sleep(1000);// 每秒20帧֡
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				if (os != null) {
+					try {
+						os.close();
+					} catch (Exception ioe) {
+					}
+				}
+				if (socket != null) {
+					try {
+						socket.close();
+					} catch (IOException e) {
+					}
+				}
+			}
+		}
+	}
+}
---
+++ RemoteMonitor/src/Server/Constant.java
@@ -0,0 +1,8 @@
+package ServerWhat;
+
+public class Constant {
+	//客户端连接服务器的socket地址
+	public static final String ADDR = "127.0.0.1";
+	public static final int SP_WIDTH = 985;
+	public static final int SP_HEIGHT = 550;
+}
