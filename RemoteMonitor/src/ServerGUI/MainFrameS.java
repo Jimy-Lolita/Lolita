eca8055eaf82d6d6d27bf6f666f03d8f62f989d8
Trrinity <522268397@qq.com>
2016-11-07 08:42:05 +0800

    多桌面监视
    之前不小心把 “远程监视桌面传输图片”的代码写在“教师端的主界面”上，现在换回来。。。代码更新也比较多：
    实现了多桌面监视跟文件传输。保存jar包时为了防止图片丢失，需要更改加入图片的代码。。

--- RemoteMonitor/src/ServerGUI/MainFrameS.java
+++ RemoteMonitor/src/ServerGUI/MainFrameS.java
@@ -1,134 +1,250 @@
 package ServerGUI;
 
-/*其中client为客户端（控制端），servlet为服务端（被控制端），
- * 直接运行两个jar文件即可。（运行jar文件必须在电脑安装jdk。）
- * 接受截图
-*/
+/*
+ * 服务器主界面
+ */
+
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.ServerSocket;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.zip.ZipInputStream;
 
-import javax.imageio.ImageIO;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
-//屏幕监控的显示框用JButton,直接用一个setImageIcon(imaegIcon)就可以显示图片，就不用重画JFrame的背景
-public class  MainFrameS  extends JButton implements Runnable{  
-    private static final long serialVersionUID = 1L;  
-    Dimension screenSize;  
-    private ServerSocket imgss;
-    private Socket imgs;
-    public  MainFrameS () {  
-    	//this.setSize(width, height);//设置按钮的大小
-    	try {
-			imgss = new ServerSocket(5001);
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import Server.FileServer;
+import Server.NowTime;
+import Server.RemoteMonitored;
+
+import Server.Screenshots;
+import Server.ServerProcess;
+
+public class MainFrameS extends JFrame implements Runnable {
+
+	// 单例模式，此类只能被实例化一次
+	private static MainFrameS mfs;
+	
+	private BufferedReader reader;
+	private PrintWriter writer;
+	private Socket socket;
+	 //public static String path =null;
+	static Socket s;
+	
+	// 对应button[6~56]，用作标记，记录button监控的ip
+	private String ip1[] = new String[51];
+	
+	JTextArea Viewtext = new JTextArea(7, 20);
+	JTextField Sendtext = new JTextField(20);
+
+	private static final long serialVersionUID = 1L;
+	Dimension screenSize;
+	
+	// button数组变为类的局部变量，让类的所有方法都能引用
+	private JButton[] button;
+	
+	// 单例模式，此类只能被new一次，只有一个实例，其它类只能通过这个方法访问该实例
+	public static MainFrameS getInstance(String name) {
+		if(mfs == null) {
+			//没有实例化过，有实例化过直接返回实例
+			mfs = new MainFrameS(name);
+		}
+		return mfs;
+	}
+	
+	// 单例模式，构造方法私有化，只能通过getInstance方法被调用，维持整个类只有一个实例
+	private MainFrameS(String name) {
+		
+		JPanel[] panel = new JPanel[10];
+		button = new JButton[100];  //显示监控的按钮?
+		JLabel[] label = new JLabel[100];
+		ImageIcon[] icon = new ImageIcon[100];
+
+		Container cp = null;
+
+		panel[1] = new JPanel(new GridLayout(1, 0));// 菜单
+		panel[1].setPreferredSize(new Dimension(685, 175));
+		panel[2] = new JPanel(new GridLayout(10, 4));// 显示桌面
+		panel[3] = new JPanel();// 文本输入 + time
+		panel[4] = new JPanel();// 文本输入
+		panel[5] = new JPanel();// time
+		
+		button[1] = new JButton();
+		Icon runImg1 = new ImageIcon(this.getClass().getClassLoader().getResource("images/001.jpg"));
+		button[1].setIcon(runImg1);
+		
+		button[2] = new JButton(" ");
+		Icon runImg2 = new ImageIcon(this.getClass().getClassLoader().getResource("images/002rm.jpg"));
+		button[2].setIcon(runImg2);
+
+		button[3] = new JButton(" ");
+		Icon runImg3 = new ImageIcon(this.getClass().getClassLoader().getResource("images/003shots.jpg"));
+		button[3].setIcon(runImg3);
+		button[3].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Screenshots sr = new Screenshots();
+			}
+		});
+
+		button[4] = new JButton(" ");
+		Icon runImg4 = new ImageIcon(this.getClass().getClassLoader().getResource("images/004file.jpg"));
+		button[4].setIcon(runImg4);
+		button[4].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FileServer sr = new FileServer(s);
+				sr.choose();
+				sr.start();
+			}
+		});
+
+		button[5] = new JButton(" ");
+		Icon runImg5 = new ImageIcon(this.getClass().getClassLoader().getResource("images/001.jpg"));
+		button[5].setIcon(runImg5);
+
+		for (int i = 6; i < 57; i++) {
+			button[i] = new JButton();
+		}
+
+		cp = getContentPane();
+
+		cp.add(panel[1], BorderLayout.NORTH);
+		JScrollPane scrollPane = new JScrollPane(panel[2]);//放置监控的滚动面板
+		scrollPane.setBounds(10, 10, 675, 200);
+		panel[2].setPreferredSize(new Dimension(985, 2750));
+		scrollPane.setLocation(0, 153);
+		cp.add(scrollPane);
+
+		for (int i = 1; i < 6; i++) {
+			panel[1].add(button[i]);
+		}
+
+		//panel[6].setLayout(null);
+		//rmi.setBounds(0, 0, panel[6].getWidth(), panel[6].getHeight());
+		//panel[6].add(rmi);
+
+		for (int i = 6; i < 55; i++) {
+			panel[2].add(button[i]);
+		}
+
+		Thread t = new Thread(new RemoteMonitored());
+		t.start();
+		
+		cp.add(panel[3], BorderLayout.SOUTH);
+
+		panel[3].add(panel[4]);
+		panel[3].add(panel[5]);
+		panel[4].add(Viewtext, BorderLayout.NORTH);
+		panel[4].add(Sendtext, BorderLayout.SOUTH);
+		Viewtext.setLocation(4, 4);
+		Sendtext.setLocation(0, 4);
+
+		Sendtext.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				writer.println("Teacher：" + Sendtext.getText());
+				Sendtext.setText("");
+			}
+		});
+
+		NowTime nowtime = new NowTime();
+		panel[5].add(nowtime);
+
+		this.setSize(703, 700);
+		this.getSocket();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+
+	}
+	
+	// 这个方法是交给线程来触发监控图片改变的
+	public void setButtonWin(String nip, Image img) {
+		int winnum = 0; //当前正在监控的窗口的数量
+		for(int i = 0; i < ip1.length; i++) {
+			if(ip1[i] != null && !ip1[i].equals("")) {
+				if(nip.equals(ip1[i])){
+					//FIXME 在已有监控窗口的button中找到ip一样的，更新这个窗口
+					winnum = i + 6;
+					break;
+				}
+			} else {
+				//比较到这里，发现有一个button没有做上已有监控窗口的标志（ip），就用这个button进行监控
+				ip1[i] = nip; //button作标记
+				winnum = i + 6;
+				break;
+			}
+		}
+		//button.add
+		//FIXME 改图片
+		button[winnum].setIcon(new ImageIcon(img));
+		
+		System.out.println("窗口号为：" + (winnum - 6));
+		System.out.println("lalala服务端识别的ip:" + nip);
+	}
+
+	public static void main(String args[]) {
+		ServerProcess server = new ServerProcess();
+		FileServer sr = new FileServer();
+		//**************************
+		//RemoteServer rs = new RemoteServer();
+		try {
+			
+			server.open();
+			
+			//初始化服务端，不带监控窗口
+			MainFrameS.getInstance("FarContrl");
+			sr.open();
+			System.out.println("Communication1");
+		
+			//rs.setup(9999);
+			//System.out.println("RemoteServer1");
+			
+			s = sr.getsever();
+			System.out.println("FileServer");
+			
+			server.getServer();	
+			System.out.println("Communication2");
+			
 		} catch (IOException e) {
-			// TODO 自动生成的 catch 块
 			e.printStackTrace();
 		}
-        //super();  
-        //screenSize = Toolkit.getDefaultToolkit().getScreenSize();  
-        //this.setSize(800, 640);  
-        //Screen p = new Screen();  
-        //Container c = this.getContentPane();  
-        //c.setLayout(new BorderLayout());  
-        //c.add(p, SwingConstants.CENTER);  
-        //new Thread(p).start();  
-        //SwingUtilities.invokeLater(new Runnable(){  
-            //public void run() {  
-            //    setVisible(true);  
-          //  }});  
-    }  
-    /*public static void main(String[] args) {  
-        new  RemoteMonitored ();  
-    }  
-  
-    class Screen extends JPanel implements Runnable {  
-  
-        private static final long serialVersionUID = 1L;  
-        private Image cimage;  
-  
-        public void run() {  
-            ServerSocket ss = null;  
-            try {  
-                ss = new ServerSocket(5001);// 探听5001端口的连接  
-                while (true) {  
-                    Socket s = null;  
-                    try {  
-                        s = ss.accept();  
-                        ZipInputStream zis = new ZipInputStream(s  
-                                .getInputStream());  
-                        zis.getNextEntry();  
-                        cimage = ImageIO.read(zis);// 把ZIP流转换为图片  
-                        repaint();  
-                    } catch (Exception e) {  
-                        e.printStackTrace();  
-                    } finally {  
-                        if (s != null) {  
-                            try {  
-                                s.close();  
-                            } catch (IOException e) {  
-                                e.printStackTrace();  
-                            }  
-                        }  
-                    }  
-                }  
-            } catch (Exception e) {  
-            } finally {  
-                if (ss != null) {  
-                    try {  
-                        ss.close();  
-                    } catch (IOException e) {  
-                        e.printStackTrace();  
-                    }  
-                }  
-            }  
-        }  
-  
-        public Screen() {  
-            super();  
-            this.setLayout(null);  
-        }  
-  
-        public void paint(Graphics g) {  
-            super.paint(g);  
-            Graphics2D g2 = (Graphics2D) g;  
-            g2.drawImage(cimage, 0, 0, null);  
-        }  
-    }  */
-//在线程中不断获取图片
-	@Override
+
+	}
+
 	public void run() {
-		// TODO 自动生成的方法存根
-		while(true){
+		while (true) {
 			try {
-				while((imgs = imgss.accept()) != null){
-					System.out.println("端口5001有连接");
-					InputStream is = imgs.getInputStream();
-					Image i = ImageIO.read(is);
-					if(i != null){
-						System.out.println("图片接受成功");
-						//对图像进行压缩，使得图片符合JButton大小
-						i = i.getScaledInstance(this.getWidth(), this.getHeight(), Image.SCALE_SMOOTH);
-						this.setIcon(new ImageIcon(i));
-					}
-				}
-			} catch (IOException e) {
-				// TODO 自动生成的 catch 块
+				Viewtext.append(reader.readLine() + "\n");
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
-}  
+
+	private void getSocket() {
+		Viewtext.append("Try to connect to server\n");
+		try {
+			socket = new Socket("localhost", 7788);
+			Viewtext.append("You can talk with your student now\n");
+			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			writer = new PrintWriter(socket.getOutputStream(), true);
+			new Thread(this).start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
